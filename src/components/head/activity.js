57c12764679adaeee6863eeed774d3e7d2c737e3
tinyCai <2110572225@qq.com>
2019-07-11 09:25:17 +0800

    1.显示美团首页头部布局

--- src/components/head/activity.js
+++ src/components/head/activity.js
@@ -5,15 +5,34 @@ import './activity.scss'
 class Activity extends Component {
   constructor() {
     super(...arguments)
-    this.state= {
+    this.state = {
+      activities: [
+        {
+          total: 48,
+          cut: 10
+        },
+        {
+          total: 58,
+          cut: 20
+        },
+        {
+          total: 100,
+          cut: 30
+        }
+      ]
     }
   }
+  getLines(arr) {
+    return arr.map((item) => `满${item.total}减${item.cut}`).join(';')
+  }
   render () {
+    const {activities} = this.state
     return (
+      
       <View className='activity'>
-        <Text>减</Text>
-        <Text>减</Text>
-        <Text>减</Text>
+        <Text className='type'>减</Text>
+        <Text>{this.getLines(activities)}</Text>
+        <Text className='length'>{activities.length}个活动</Text>
       </View>
     )
   }
