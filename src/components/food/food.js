1a14e36a9f22c011764c3d2987f5dd6e0559aa97
tinyCai <2110572225@qq.com>
2019-07-18 19:36:43 +0800

    1.实现菜单数据渲染

--- src/components/food/food.js
+++ src/components/food/food.js
@@ -4,7 +4,6 @@ import { AtTabs, AtTabsPane } from 'taro-ui'
 import Cate from './cate'
 import List from './list'
 import './food.scss'
-import { array } from 'prop-types';
 
 class Food extends Component {
   constructor() {
@@ -13,7 +12,8 @@ class Food extends Component {
       current: 0,
       tabList: [{title: '点菜'}, {title: '评价'},{title: '商家'}],
       currentList: [],
-      foodLists: []
+      foodLists: [],
+      selectedCate: null
     }
   }
   // 监听Tab标签切换事件
@@ -22,35 +22,36 @@ class Food extends Component {
   }
   // 监听分类变化
   handleCateChange(selectedCate) {
+    this.setState({selectedCate: selectedCate})
     if (this.state.foodLists.some(item => item.id === selectedCate)) {
       this.setState({currentList:this.state.foodLists.filter(item => item.id === selectedCate)})
     } else {
-      this.setState({current: this.state.foodLists.concat(this.getData(selectedCate))}, () => {
+      this.setState({foodLists: this.state.foodLists.concat(this.getData(selectedCate))}, () => {
         this.setState({currentList:this.state.foodLists.filter(item => item.id === selectedCate)})
       })
     }
   }
   // 获取数据
   getData(selectedCate) {
-    let imgUrl = `../../assets/images/${Math.round(Math.random()*2)}.jpg`
-    return Array.from(Array(Math.round(Math.random()*20)), (v,i) => {
+    return Array.from(Array(Math.round(Math.random()*6)), (v,i) => {
       return {
-        title: "分类" + selectedCate + "菜品" + (i + 1),
+        name: '分类' + selectedCate,
+        title: '菜品_' + (i + 1),
         id: selectedCate,
-        imgUrl: imgUrl,
+        imgUrl: Math.round(Math.random()*2),
         sole: Math.round(Math.random()*50),
         price: Math.round(Math.random()*20)
       }
     })
   }
   render() {
-    const {current, tabList, currentList} = this.state
+    const {current, tabList, currentList, selectedCate} = this.state
     return (
       <View className='food'>
         <AtTabs current={current} tabList={tabList} onClick={this.handleTabChange.bind(this)}>
           <AtTabsPane>
             <View className='food_box'>
-              <Cate onChangeCate={this.handleCateChange.bind(this)} /> <List currentList={currentList} />
+              <Cate onChangeCate={this.handleCateChange.bind(this)} /> <List selectedCate={selectedCate} currentList={currentList} />
             </View>
           </AtTabsPane>
         </AtTabs>
--- src/components/food/list.js
+++ src/components/food/list.js
@@ -1,12 +1,27 @@
 import Taro,{Component} from '@tarojs/taro'
-import {View} from '@tarojs/components'
+import {View, Image, Text} from '@tarojs/components'
+import './list.scss'
 
 class List extends Component {
   render() {
     console.log(this.props.currentList)
+    const {currentList, selectedCate} = this.props
     return (
       <View className='list'>
-        菜单列表
+        <Text>{selectedCate}</Text>
+        {
+           currentList.map((item) => {
+            return <View key={item.id} className='foodList_item'>
+               <Image className='foodList_pic' src={item.imgUrl < 2 ? require('../../assets/images/2.jpg') : require('../../assets/images/1.jpg')}></Image>
+               <View className='foodList_info'>
+                 <Text>{item.title}</Text>
+                 <Text>销量：{item.sole}</Text>
+                 <Text className='price'>￥{item.price}</Text>
+               </View>
+            </View>
+           
+          })
+        }
       </View>
     )
   }
